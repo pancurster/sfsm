1ed246dc56e31a6f23c2e503da80a110f7edc163
Lukasz Panek <pancurster@gmail.com>
2014-07-13 10:34:25 +0200

    Core changes

--- sfsm.c
+++ sfsm.c
@@ -1,8 +1,74 @@
 #include "sfsm.h"
+#include <malloc.h>
+#include <assert.h>
+#include <string.h>
+
+static void fsm_change_state(fsm_t* fsm, int st_id)
+{
+    if (fsm->state[fsm->in_state].on_exit)
+        fsm->state[fsm->in_state].on_exit(fsm);
+
+    /* for now we assume that state id can't be higher than number of states */
+    assert(st_id <= fsm->nstates);
+    fsm->in_state = fsm->id2state[st_id];
+
+    if (fsm->state[fsm->in_state].on_enter)
+        fsm->state[fsm->in_state].on_enter(fsm);
+}
+
+static void map_id2state(const state_t* statetab, const int snum, int* smap)
+{
+    int i;
+    for (i=0; i < snum; i++)
+    {
+        assert(statetab[i].id <= snum);
+        smap[statetab[i].id] = i;
+    }
+}
+
+static state_t* get_raw_state(fsm_t* fsm, int sid)
+{
+    return &fsm->state[fsm->id2state[sid]];
+}
+
+static state_t* get_acc_state(fsm_t* fsm)
+{
+    return get_raw_state(fsm, fsm->in_state);
+}
+
+void fsm_init(fsm_t* fsm, state_t* statetab, int nstates)
+{
+    memset(fsm, 0, sizeof(fsm));
+
+    fsm->state = statetab;
+    fsm->nstates = nstates;
+    fsm->in_state = 0;
+
+    fsm->id2state = malloc(sizeof(int) * nstates);
+    map_id2state(fsm->state, fsm->nstates, fsm->id2state);
+}
 
 void fsm_run(fsm_t* fsm)
 {
-    fsm->p_state = 0;
-    fsm->state[fsm->p_state].on_enter(fsm);
+    fsm->in_state = 0;
+    fsm->state[fsm->in_state].on_enter(fsm);
+}
+
+void fsm_go_state(fsm_t* fsm, int st_id)
+{
+    fsm_change_state(fsm, st_id);
+}
+
+void fsm_go_default(fsm_t* fsm)
+{
+    assert(fsm->state[fsm->in_state].default_transition_state_id != FSM_NO_DEF);
+
+    if (fsm->state[fsm->in_state].default_transition_state_id != FSM_NO_DEF)
+        fsm_change_state(fsm, fsm->state[fsm->in_state].default_transition_state_id);
+}
+
+void* fsm_get_state_data(fsm_t* fsm)
+{
+    return get_acc_state(fsm)->data;
 }
 
--- sfsm.h
+++ sfsm.h
@@ -1,7 +1,7 @@
 #ifndef _SFSM_H_
 #define _SFSM_H_
 
-#endif//_SFSM_H_
+#define FSM_NO_DEF (-1)
 
 typedef struct fsm_t_ fsm_t;
 typedef struct state_t_ state_t;
@@ -11,10 +11,12 @@ struct fsm_t_ {
     state_t* state;             /* states table */
     int nstates;                /* numer of states in states tabel */
 
-    int p_state;                /* present state */
+    int in_state;                /* present state */
+    int* id2state;              /* maps states id to state index in table */
 };
 
 struct state_t_ {
+    int id;
     void (*on_enter)(fsm_t* this);
     void (*on_exit)(fsm_t* this);
 
@@ -22,5 +24,11 @@ struct state_t_ {
     void* data;                 /* state data */
 };
 
+void fsm_init(fsm_t* fsm, state_t* statetab, int nstates);
 void fsm_run(fsm_t* fsm);
+void fsm_go_state(fsm_t* fsm, int st_id);
+void fsm_go_default(fsm_t* fsm);
+void* fsm_get_state_data(fsm_t* fsm);
+
+#endif//_SFSM_H_
 
